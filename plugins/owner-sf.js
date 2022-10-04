282408196045dc67c592a0ed8ba007bf941984b7
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-10-04 15:39:49 +0500

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
+    if (!text) throw `uhm.. Where Is The Text?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
     if (!m.quoted.text) throw `balas pesan nya!`
     let path = `${text}`
     await fs.writeFileSync(path, m.quoted.text)
-    m.reply(`tersimpan di ${path}`)
+    m.reply(`Saved In${path}`)
 }
 handler.help = ['sf'].map(v => v + ' <teks>')
 handler.tags = ['owner']
