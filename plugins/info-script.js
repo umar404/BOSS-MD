2b1047f77460709b30f083bbfbbdb3356b3ff88b
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-10-07 15:31:37 +0500

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,16 +1,16 @@
-//Credits Jangan Dihapus
-//Thanks To Papah-Chan
+//Credits Dark Devil Umar
+//Thanks To Umar
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/umar404/Boss-Md-Bot*`
+let str = `*https://github.com/umar404/BOSS-MD*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://wa.me/923165123719",
     title: 'Boss-Md-Bot',
-    body: 'want source code?',
+    body: 'BOSS UMAR DEVIL?',
     thumbnail: thumb,
     sourceUrl: sig
   }
