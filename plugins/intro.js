6190d391641c77164ea396cc84949aa9abdee217
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-10-07 15:55:40 +0500

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -1,20 +1,20 @@
 let handler = async m => {
 
-let krtu = `Kartu Intro`
+let krtu = `Umar Intro`
 m.reply(`
 0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
 │       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
- |  *Status     :* 
+│ *Name    :Umar Rehman* 
+│ *Gender   :Male:* 
+│ *Age    :16* 
+│ *Hobby    :Free Fire And Bot Script:* 
+│ *Life Ranked    :Single:* 
+│ *City         :Rawalpindi:* 
+│ *Religion   :Islam * 
+ |  *Phone Num :+923165123719:
 ╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
-`.trim()) // Tambah sendiri kalo mau
-}
+`.trim()) // Add Not Your Self 😹
+3}
 handler.command = /^(intro)$/i
 
 export default handler
